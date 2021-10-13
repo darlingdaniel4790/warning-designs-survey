fec9fee009e97a880716531766ab9797a8a0e20e
Darlington Daniel <darlingdaniel4790@gmail.com>
2021-10-13 20:17:02 +0300

    small changes

--- pages/index.js
+++ pages/index.js
@@ -33,31 +33,45 @@ export default function Home() {
           <br />
           <br />
           <Typography variant="h5">
-            Brief Introduction As smart working becomes inevitable due to
-            several reasons e.g., cost, covid etc. Users must be careful in
-            making the right choice on the internet especially with the upsurge
-            of cybercriminals. We are conducting a short survey with users on
-            how they take critical decisions during online activities. The
-            online survey should take you maximum [15 minutes]. You will at a
-            certain point be asked to assume some roles e.g., secretary, sales
-            representative, or an employee. The study aims at analyzing user
-            perception on certain scenario. There are three (3) short sections
-            A, B, and C. In all sections you will be expected to choose an
-            option or provide short answers Section A (getting to know you) In
-            this section, we save you the time of introducing yourself and ask
-            you simple getting to know you chats. Section B1 main sections Here,
-            you will be asked to assume a specific role e.g., manager,
+            As smart working becomes inevitable due to several reasons e.g.,
+            cost, covid etc. Users must be careful in making the right choice on
+            the internet especially with the upsurge of cybercriminals. <br />
+            <br />
+            We are conducting a short survey with users on how they take
+            critical decisions during online activities. The online survey
+            should take you maximum [15 minutes]. You will at a certain point be
+            asked to assume some roles e.g., secretary, sales representative, or
+            an employee. The study aims at analyzing user perception on certain
+            scenario. <br />
+            <br />
+            There are three (3) short sections A, B, and C. In all sections you
+            will be expected to choose an option or provide short answers.{" "}
+            <br />
+            <br />
+            Section A (getting to know you) <br />
+            In this section, we save you the time of introducing yourself and
+            ask you simple getting to know you chats. <br />
+            <br />
+            Section B1 (main sections) <br />
+            Here, you will be asked to assume a specific role e.g., manager,
             secretary, or company representative. We are interested on the
             advice, decision, and inferences you will take in each situation.
             Very simple and direct questions follow immediately. Please be
-            careful to read the text on the picture image carefully. Section B2
-            is similar to section B1 but will also require that you observe the
-            picture images carefully. Section B3 is similar to section B2 but
-            will also require that you observe the picture images carefully.
-            Section C Demographics Final notes This is not an examination. Your
-            participation in this research is voluntary. Note: At some point you
-            may notice similar question. Don’t be disturbed, you are still on
-            track.
+            careful to read the text on the picture image carefully. <br />
+            <br />
+            Section B2 is similar to section B1 but will also require that you
+            observe the picture images carefully. <br />
+            <br />
+            Section B3 is similar to section B2 but will also require that you
+            observe the picture images carefully.
+            <br />
+            <br />
+            Section C Demographics <br />
+            Final notes This is not an examination. Your participation in this
+            research is voluntary. <br />
+            <br />
+            Note: At some point you may notice similar question. Don’t be
+            disturbed, you are still on track.
           </Typography>
           <br />
           <br />
