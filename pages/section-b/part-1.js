dbde89a7c291d49056cb50d901b1d3baaed839ad
Josephogo <josephogo.aneke@gmail.com>
2021-10-13 19:48:08 +0200

    joe changes

--- pages/section-b/part-1.js
+++ pages/section-b/part-1.js
@@ -429,11 +429,7 @@ const Part1 = (props) => {
               <Grid item>
                 <Paper elevation={5} className={classes.questions}>
                   <Typography variant="h4">
-                    Ms. white is a new secretary of a popular logistic company
-                    (name not shown due to privacy) she is presently activating
-                    user accounts and assigning new orders. She clicked orders
-                    A54 received on 8/07/2021 and it returned the following
-                    message.
+                  Activation of new sale representative accounts
                   </Typography>
                 </Paper>
               </Grid>
