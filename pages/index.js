2c26f2dcf6043f67e45c3e6766491e119179d067
Josephogo <josephogo.aneke@gmail.com>
2021-12-04 00:08:07 +0100

    joe

--- pages/index.js
+++ pages/index.js
@@ -54,7 +54,7 @@ export default function Home() {
           <Typography variant="h5">
             
             We are conducting a short survey on how people take certain decisions during online activities. <br />
-            The online survey should take you a maximum [15 minutes]. You will at a certain point assume some roles e.g., secretary, sales representative, or an employee <br />
+            The online survey should take you a maximum [15 minutes]. You will at a certain point assume some roles e.g., a new user, an applicant, etc. <br />
             <br />
             This is not an examination. Your participation in this research is
             voluntary.
